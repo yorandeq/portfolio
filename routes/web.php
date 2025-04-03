d4d93dc6912bb4010b0857538f79eddf5a46df08
yoranpingmeedia <yoran.dequay@pingmedia.nl>
2025-04-03 21:24:26 +0200

    Added portfolio startup

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,29 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/', function () {
+    // $jobs = Job::all();
+
     return view('home');
 });
-
+ 
 Route::get('/jobs', function () {
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => Job::all()
     ]);
+});
+
+Route::get('/job/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/job/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('jobs', function() {
+    dd(request->all());
 });
 
 Route::get('contact', function () {
